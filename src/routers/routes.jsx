f6110746eaa686d63084daa0f2facdea5e625303
casilimas <freddycasilimas@gmail.com>
2025-05-25 15:30:48 -0400

    Usar HashRouter para evitar error 404 en Vercel

--- src/routers/routes.jsx
+++ src/routers/routes.jsx
@@ -1,6 +1,7 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import NavBar from "../components/NavBar";
 
+import { HashRouter as Router } from "react-router-dom";
 
 
 
